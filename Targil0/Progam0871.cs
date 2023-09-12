ef98fc3e5133ecd0d59385bfdfc5b868728f6557
Chavi haimson <c44362@gmail.com>
2023-09-12 17:07:20 +0300

    adding also my function

---
+++ Targil0/Progam0871.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Progam
+    {
+        static partial void Welcome0871()
+        {
+            Console.WriteLine("I am here");
+        }
+    }
+}
