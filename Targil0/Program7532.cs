653216d96d5e5315655e07b13d325d52116c634e
Chani Levy <chani86370@gmail.com>
2023-09-12 16:18:00 +0300

    initial code + welcome function

---
+++ Targil0/Program7532.cs
@@ -0,0 +1,20 @@
+﻿namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome7532();
+            Welcome0871();
+            Console.ReadKey();
+        }
+
+        private static void Welcome7532()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+        static partial void Welcome0871();
+    }
+}
