6823c9926c0fd19e29037352d289ec06007183a3
Chavi Chaimson <c44362@gmail.com>
2023-11-07 15:26:00 +0200

    finishing info of engineer

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -17,17 +17,17 @@ class Program
     {
         switch (x)
         {
-            
+
             case 'a'://add                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            t = new Task();
                 Console.WriteLine("enter alias's id to add");
                 string alias = Console.ReadLine();
                 Console.WriteLine("enter task's name");
-                string description =Console.ReadLine();
-                DO.Task task=new(4,description,alias);
+                string description = Console.ReadLine();
+                DO.Task task = new(4, description, alias);
                 try
                 {
-     
-                    int result= ITask.create(p);
+
+                    int result = ITask.create(p);
                     Console.WriteLine("the task was added");
                 }
                 catch (Exception ex)
@@ -107,11 +107,11 @@ class Program
                 string name = Console.ReadLine()!;
                 Console.WriteLine("enter engineer's email");
                 string email = Console.ReadLine()!;
-                DO.Engineer myEngineer= new(id, name, email);
+                DO.Engineer myEngineer = new(id, name, email);
                 try
                 {
                     int result = s_dalEngineer.Create(myEngineer);
-                    Console.WriteLine("the task was added");
+                    Console.WriteLine("the engineer was added");
                 }
                 catch (Exception ex)
                 {
@@ -119,11 +119,11 @@ class Program
                 }
                 break;
             case 'b'://read by id
-                Console.WriteLine("enter product's id to read");
-                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter engineer's id to read");
+                int idRead = int.Parse(Console.ReadLine());
                 try
                 {
-                    Console.WriteLine(dalProduct.read(id));
+                    Console.WriteLine(s_dalEngineer.Read(idRead));
                 }
                 catch (Exception ex)
                 {
@@ -131,51 +131,60 @@ class Program
                 }
                 break;
             case 'c'://read all
-                Console.WriteLine("all the tasks:");
-                Product[] arrReadAllTasks = dalProduct.readAll();
-                foreach (var item in arrReadAllProducts)
+                Console.WriteLine("all the engineers:");
+                List<DO.Engineer> arrReadAllEngineers = s_dalEngineer.ReadAll();
+                foreach (var item in arrReadAllEngineers)
                     Console.WriteLine(item);
                 break;
-            case 'd'://update
-                Console.WriteLine("enter id of product to update");
-                int idUpdate = int.Parse(Console.ReadLine());//search of the id to update
-                try
-                {
-                    Console.WriteLine(dalProduct.read(idUpdate));
-                    Product pUpdate = new Product();
-                    pUpdate._id = idUpdate;
-                    Console.WriteLine("enter product's name");
-                    pUpdate._name = Console.ReadLine();
-                    Console.WriteLine("enter product's price");
-                    pUpdate._price = double.Parse(Console.ReadLine());
-                    Console.WriteLine("enter product's category(0-for cups,1-for cakes,2-for cookies)");
-                    pUpdate._category = (ECategory)int.Parse(Console.ReadLine());
-                    Console.WriteLine("enter product's instock");
-                    pUpdate._inStock = int.Parse(Console.ReadLine());
-                    Console.WriteLine("enter product's parve(0/1)");
-                    pUpdate._parve = int.Parse(Console.ReadLine());
-                    dalProduct.update(pUpdate);
+        }
 
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
-                break;
-            case 'e'://delete a product
-                Console.WriteLine("enter id of product to delete");
-                int idDelete = int.Parse(Console.ReadLine());
+
+            case 'd'://update
+            Console.WriteLine("enter id of engineer to update");
+            int idUpdate = int.Parse(Console.ReadLine());//search of the id to update
+            try
+            {
+                Console.WriteLine("enter engineer's id to update");
+                int id = int.Parse(Console.ReadLine()!);
+                Console.WriteLine("enter engineer's name");
+                string name = Console.ReadLine()!;
+                Console.WriteLine("enter engineer's email");
+                string email = Console.ReadLine()!;
+                Console.WriteLine("enter engineer's level from 0- to 2");
+                int? level=int.Parse(Console.ReadLine()!);
+                EngineerExperience? enLevel;
                 try
                 {
-                    dalProduct.delete(idDelete);
+                    enLevel = (EngineerExperience)level;
                 }
-                catch (Exception ex)
+                catch
                 {
-                    Console.WriteLine(ex);
+                    Console.WriteLine("I tell you to put between 0 to 2");
                 }
-                break;
-            default:
-                break;
+                Console.WriteLine("enter engineer's cost");
+                double cost=double.Parse(Console.ReadLine()!);
+                DO.Engineer myEngineer = new(id, name, email, enLevel, cost);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            break;
+        case 'e'://delete a product
+            Console.WriteLine("enter id of product to delete");
+            int idDelete = int.Parse(Console.ReadLine());
+            try
+            {
+                dalProduct.delete(idDelete);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            break;
+        default:
+            break;
         }
     }
     public static void Main(string[] args)
@@ -184,7 +193,7 @@ class Program
         try
         {
 
-            Initialization.Do(s_dalTask,s_dalDependency,s_dalEngineer);
+            Initialization.Do(s_dalTask, s_dalDependency, s_dalEngineer);
         }
         catch
         {
