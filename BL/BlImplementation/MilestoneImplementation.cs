4e7856beea3119b528e1d342e4fb85afe58ffd86
Chavi <chavideveloper@gmail.com>
2024-01-23 21:09:03 +0200

    starting work on create milestone

--- BL/BlImplementation/MilestoneImplementation.cs
+++ BL/BlImplementation/MilestoneImplementation.cs
@@ -10,13 +10,16 @@ namespace BlImplementation
         private DalApi.IDal _dal = DalApi.Factory.Get;
         public void CreateProjectSchedule()
         {
-            //var taskDEpendency = (from dependency in recievedDeps
-            //                             where dependency?.DependentTask != null && dependency?.DependsOnTask != null
-            //                             group dependency by dependency.DependentTask
-            //                          into dependencyListAfterGrouping
-            //                             let dependencyList = (from dependency in dependencyListAfterGrouping
-            //                                                   select dependency.DependsOnTask).Order()
-            //                             select new { _key = dependencyListAfterGrouping.Key, _value = dependencyList }).ToList();
+            var tasks = _dal.Task.ReadAll();
+            var dependencyList=_dal.Dependency.ReadAll();
+
+            var taskDEpendency = (from dependency in dependencyList
+                                  where dependency?.DependentTask != null && dependency?.DependensOnTask != null
+                                  group dependency by dependency.DependentTask
+                                      into dependencyListAfterGrouping
+                                  //let dependencyList = (from dependency in dependencyListAfterGrouping
+                                  //                      select dependency.DependsOnTask).Order()
+                                  //select new { _key = dependencyListAfterGrouping.Key, _value = dependencyList }).ToList();
         }
 
         public Milestone? Read(int id)
@@ -28,5 +31,6 @@ namespace BlImplementation
         {
             throw new NotImplementedException();
         }
+        
     }
 }
